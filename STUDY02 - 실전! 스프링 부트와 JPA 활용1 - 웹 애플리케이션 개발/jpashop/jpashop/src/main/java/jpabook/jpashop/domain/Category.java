cc8214b3d5243433e6aa85c40e53b24c7af446f5
sh.jung <sh.jung@okestro.com>
2023-04-03 15:58:08 +0900

    [DONE] ~230402 도메인 분석 설계

--- STUDY02 - 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ STUDY02 - 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -21,5 +21,18 @@ public class Category {
     @JoinTable(name="category_item",
             joinColumns= @JoinColumn(name="category_id"),
             inverseJoinColumns = @JoinColumn(name="item_id")) // 다대다를 풀어내는 방법, 실무에서는 쓰지 말 것. 필드 추가 불가능하므로.. JPA에서 되기는 함
-    private List<Item> child = new ArrayList<>();
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent") // 같은 엔티티 내에서 연관관계를 셀프로 매핑함
+    private List<Category> child = new ArrayList<>();
+
+    // 연관관계 편의 메서드
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- STUDY02 - 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ STUDY02 - 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id") // FK의 이름이 member_id가 됨
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // cascade로 Order를 persist 할 때 OrderItem 엔티티도 persist 함
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // cascade로 Order를 persist 할 때 Delivery 엔티티도 persist 함
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
@@ -35,4 +35,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문의 상태 [ORDER, CANCEL]
+
+    // 연관관계 편의 메서드
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
